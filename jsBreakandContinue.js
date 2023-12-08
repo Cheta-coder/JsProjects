04e024ede413eec8762b99f376454cfe843e607d
Cheta-coder <happinessugwuanyi7@gmail.com>
2023-12-08 09:47:32 +0100

    finished the population

--- jsBreakandContinue.js
+++ jsBreakandContinue.js
@@ -13,40 +13,40 @@ let dataCollect = []
 
 // Populate the data into a table
 let table = document.createElement('table');
+
+let trhead = document.createElement('tr')
+let th1 = document.createElement('th')
+let th2 = document.createElement('th')
+let th3 = document.createElement('th')
+let th4 = document.createElement('th')
+
+th1.innerText = 'Course Name'
+th2.innerText = 'Course Price'
+th3.innerText = 'Course Duration'
+th4.innerText = 'Course Instructor'
+
+trhead.append(th1, th2, th3, th4,)
+table.append(trhead)
+
 let populate = ()=>{
+
+    let trow1 = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+
+    td1.innerHTML = dataCollect[0];
+    td2.innerHTML = dataCollect[1];
+    td3.innerHTML = dataCollect[2];
+    td4.innerHTML = dataCollect[3];
+
+    trow1.append(td1, td2, td3, td4);
+    table.append(trow1);
     
-    let trhead = document.createElement('tr')
-    let th1 = document.createElement('th')
-    let th2 = document.createElement('th')
-    let th3 = document.createElement('th')
-    let th4 = document.createElement('th')
-
-    th1.innerText = 'Course Name'
-    th2.innerText = 'Course Price'
-    th3.innerText = 'Course Duration'
-    th4.innerText = 'Course Instructor'
-
-    trhead.append(th1, th2, th3, th4,)
-    table.append(trhead)
-
-    for(i = 0; i<dataCollect.length; i++){
-        let trow1 = document.createElement('tr');
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        let td3 = document.createElement('td');
-        let td4 = document.createElement('td');
-
-        td1.innerHTML = dataCollect[0];
-        td2.innerHTML = dataCollect[1];
-        td3.innerHTML = dataCollect[2];
-        td4.innerHTML = dataCollect[3];
-
-        trow1.append(td1, td2, td3, td4);
-        table.append(trow1,);
-        
-    }
     console.log(table)
-        tableDiv.innerHTML = ""
+    tableDiv.innerHTML = ""
+    tableDiv.innerHTML = ''
     tableDiv.append(table)
 }
 
